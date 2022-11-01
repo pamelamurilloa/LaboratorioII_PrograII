71dfa17663a5d9bab4f8ec3fe9125db8d93ba86b
Pamela Murillo <pamelamurilloa@gmail.com>
2022-11-01 15:18:06 -0600

    Main start ready

---
+++ LaboratorioII_Futbol/src/MainStart/Start.java
@@ -0,0 +1,16 @@
+package MainStart;
+
+import Presentation.InsertPlayers;
+
+
+public class Start {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InsertPlayers mainWindow = new InsertPlayers();
+        mainWindow.setVisible(true);
+    }
+    
+}
